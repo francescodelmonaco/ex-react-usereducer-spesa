4c9f258e401c5bc3cb5e92c6944f83bf800d4584
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-05-20 11:18:29 +0200

    add: milestone 2

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function ProductsList() {
     const products = [
         { name: 'Mela', price: 0.5 },
@@ -5,6 +7,20 @@ export default function ProductsList() {
         { name: 'Latte', price: 1.0 },
         { name: 'Pasta', price: 0.7 },
     ];
+
+    const [addedProducts, setAddedProducts] = useState([]);
+    console.log(addedProducts);
+
+
+    const addToCart = product => {
+        const pIsAdded = addedProducts.some(p => p.name === product.name);
+
+        if (pIsAdded) {
+            return;
+        };
+
+        setAddedProducts(curr => [...curr, { ...product, quantity: 1 }]);
+    };
 
     return (
         <div>
@@ -14,11 +30,34 @@ export default function ProductsList() {
                 {
                     products.map((p, i) => {
                         return (
-                            <li key={i}>{p.name} / {p.price} €</li>
+                            <li key={i}>
+                                <p>{p.name} / {p.price.toFixed(2)} €</p>
+                                <button onClick={() => addToCart(p)}>Aggiungi al carrello</button>
+                            </li>
                         )
                     })
                 }
             </ul>
+
+            {
+                addedProducts.length > 0 && (
+                    <div>
+                        <h2>Carrello prodotti</h2>
+
+                        <ul>
+                            {
+                                addedProducts.map((a, i) => {
+                                    return (
+                                        <li key={i}>
+                                            <p>{a.name} / {a.price.toFixed(2)} € / Quantity: {a.quantity}</p>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                )
+            }
         </div>
-    )
+    );
 }
